45b20f4c42c6cff43f0365fb10e3936ec41b252c
Jake Cannon <jakecan13@gmail.com>
2022-10-30 15:53:55 -0400

    Move algorithms post feed to /algorithms route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Head>
       <nav>
         <Link href="/">/home</Link>
+        <Link href="/algorithms">/algorithms</Link>
       </nav>
       <Component {...pageProps} />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,8 @@
-import { getAllPosts, PostMeta } from "@/src/api";
-import Articles from "@/src/components/articles";
 
-export default function Home({ posts }: { posts: PostMeta[] }) {
+export default function Home() {
   return (
     <>
-      <h1>Articles</h1>
-      <Articles posts={posts} />
+      <h1>Home page</h1>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const posts = getAllPosts()
-    .slice(0, 9)
-    .map((post) => post.meta);
-
-  return { props: { posts } };
 }
