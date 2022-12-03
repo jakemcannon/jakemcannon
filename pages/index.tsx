48ab5590fba72fbd78c3bf3cff910005858c8d79
Jake Cannon <jakecan13@gmail.com>
2022-12-03 17:50:42 -0500

    Update path for profile pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import profilePic from "../public/profile_pic.JPG";
 
 export default function Home() {
   return (
     <>
       <h1>Home page</h1>
-      <Image src={profilePic} width={293} height={500} />
+      <Image src={"/profile_pic.JPG"} width={293} height={500} />
     </>
   );
 }
