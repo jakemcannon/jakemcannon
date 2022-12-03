b72dec4232dfa4bb4c72d7914b24796cb7ec98d1
Jake Cannon <jakecan13@gmail.com>
2022-12-03 14:45:38 -0500

    Update app structure to support SideBar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,24 @@
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import Head from "next/head";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, Center, ChakraProvider, Flex } from "@chakra-ui/react";
 
 import { theme } from "../theme";
+import SideBar from "@/src/components/SideBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container">
-      <Head>
-        <title>Jake Cannon</title>
-      </Head>
-      <nav>
-        <Link href="/">/home</Link>
-        <Link href="/algorithms">/algorithms</Link>
-        <Link href="/patterns">patterns</Link>
-      </nav>
+    <div>
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <Flex>
+          <SideBar />
+          <Flex flex="1" flexDir="column" grow={100}>
+            <Center>
+              <Box padding={1} w={[400, 500, 700, 800]} mt={5}>
+                <Component {...pageProps} />
+              </Box>
+            </Center>
+          </Flex>
+        </Flex>
       </ChakraProvider>
     </div>
   );
