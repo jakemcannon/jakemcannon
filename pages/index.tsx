44a4e52428a9b8e307a4650ec867303a24ee1819
Jake Cannon <jakecan13@gmail.com>
2022-12-03 17:44:47 -0500

    Add profile pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import profilePic from "../public/profile_pic.JPG";
 
 export default function Home() {
   return (
     <>
       <h1>Home page</h1>
+      <Image src={profilePic} width={293} height={500} />
     </>
   );
 }
